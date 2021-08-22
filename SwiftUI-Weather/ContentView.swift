746ebe6ec636b2e3a3a4de4f4abec33128af0616
John Lollar <jlollar45@gmail.com>
2021-08-21 21:01:00 -0500

    Initial Commit

---
+++ SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by ParkingPal on 8/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
